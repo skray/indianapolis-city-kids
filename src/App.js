8413a1f994484654e12c33944dfc3d22db3d70b9
sean <sean@seans-MacBook-Pro.local>
2017-09-06 23:00:37 -0400

    Breaking out flag as component, and adding red circle

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import {List, ListItem} from 'material-ui/List';
 import StarIcon from 'material-ui/svg-icons/action/grade';
-import {cyan500} from 'material-ui/styles/colors';
+import {red300} from 'material-ui/styles/colors';
 import Categories from './Categories';
 import categoryList from './categoryList';
 import './App.css';
@@ -30,6 +30,24 @@ const styles = {
     'padding': '25px 0 0 64px'
   }
 };
+
+const Flag = () => {
+  let flagHeight = 250;
+  let flagWidth = 400;
+  let lineWidth = 40;
+  let circleDiameter = 120;
+  let starDiameter = 60;
+
+  return <span style={{display: 'flex', 'flex-direction':'column', 'justify-content':'center'}}>
+    <span style={{position: 'relative', width:`${flagWidth}px`, height: `${flagHeight}px`, display: 'flex'}}>
+      <div style={{width: '100%', height: `${lineWidth}px`, 'background-color':'white', position: 'absolute', top: `${flagHeight/2 - lineWidth/2}px`}}></div>
+      <div style={{width: `${lineWidth}px`, height: '100%', 'background-color':'white', position: 'absolute', left: `${flagWidth/2 - lineWidth/2}px`}}></div>
+      <div style={{width: `${circleDiameter}px`, height: `${circleDiameter}px`, 'border-radius': '50%', 'background-color': 'white', position: 'absolute', left: `${flagWidth/2 - circleDiameter/2}px`, top: `${flagHeight/2 - circleDiameter/2}px`}}></div>
+      <div style={{width: `${starDiameter}px`, height: `${starDiameter}px`, 'border-radius': '50%', 'background-color': red300, position: 'absolute', left: `${flagWidth/2 - starDiameter/2}px`, top: `${flagHeight/2 - starDiameter/2}px`}}></div>
+      <StarIcon style={{width: `${starDiameter}px`, height: `${starDiameter}px`, 'border-radius': '50%', 'color': 'white', position: 'absolute', left: `${flagWidth/2 - starDiameter/2}px`, top: `${flagHeight/2 - starDiameter/2}px`}}/>
+    </span>
+  </span>
+}
 
 const ItemList = (props) => (
   <List>
@@ -66,11 +84,6 @@ class App extends Component {
 
   render() {
     let selectedCategory = this.state.selectedCategory;
-    let flagHeight = 250;
-    let flagWidth = 400;
-    let lineWidth = 40;
-    let circleDiameter = 90;
-    let starDiameter = 60;
 
     return (
       <MuiThemeProvider>
@@ -82,14 +95,7 @@ class App extends Component {
             />
           <div style={styles.header}>
             <span style={{width: '50%', padding: '100px 100px 100px 0'}}>Have kids in the city? Find things to do where you are, instead of having to drive to the 'burbs.</span>
-            <span style={{display: 'flex', 'flex-direction':'column', 'justify-content':'center'}}>
-              <span style={{position: 'relative', width:`${flagWidth}px`, height: `${flagHeight}px`, display: 'flex'}}>
-                <div style={{width: '100%', height: `${lineWidth}px`, 'background-color':'white', position: 'absolute', top: `${flagHeight/2 - lineWidth/2}px`}}></div>
-                <div style={{width: `${lineWidth}px`, height: '100%', 'background-color':'white', position: 'absolute', left: `${flagWidth/2 - lineWidth/2}px`}}></div>
-                <div style={{width: `${circleDiameter}px`, height: `${circleDiameter}px`, 'border-radius': '50%', 'background-color': 'white', position: 'absolute', left: `${flagWidth/2 - circleDiameter/2}px`, top: `${flagHeight/2 - circleDiameter/2}px`}}></div>
-                <StarIcon style={{width: `${starDiameter}px`, height: `${starDiameter}px`, 'border-radius': '50%', 'color': cyan500, position: 'absolute', left: `${flagWidth/2 - starDiameter/2}px`, top: `${flagHeight/2 - starDiameter/2}px`}}/>
-              </span>
-            </span>
+            <Flag />
           </div>
           <h1 style={styles.lookingForHeader}>I'm looking for...</h1>
           <Categories menuItems={this.state.categories} onClick={(tag) => this.handleClick(tag)} />
